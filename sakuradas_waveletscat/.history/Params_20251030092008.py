49a2830f440b00c913419f893cd8475a4e138b66
hiroset <167729675+hiroset0391162621@users.noreply.github.com>
2025-10-30 09:47:41 +0900

    update

---
+++ sakuradas_waveletscat/.history/Params_20251030092008.py
@@ -0,0 +1,29 @@
+import datetime
+
+
+#Fs_raw = 250.0
+segment_duration_seconds = 20.0
+sampling_rate_hertz = 100.0
+Fs = sampling_rate_hertz
+overlap = 1 ### % overlap=1 means without overlapping
+pooling = 'max'
+
+
+fiber = 'nojiri' #'round'
+
+# hdf5_starttime_jst = datetime.datetime(2023, 12, 1, 0, 0, 0)
+# hdf5_endttime_jst = datetime.datetime(2023, 12, 1, 0, 10, 0)
+
+hdf5_starttime_jst = datetime.datetime(2025, 5, 15, 0, 0, 0)
+hdf5_endttime_jst = datetime.datetime(2025, 5, 16, 0, 0, 0)
+
+Nseconds = int( (hdf5_endttime_jst-hdf5_starttime_jst).total_seconds() )
+N_minute = int( (hdf5_endttime_jst - hdf5_starttime_jst).total_seconds() / 60.0 )
+windL = 60.0*N_minute
+low_pass = 0.5
+high_pass = 50
+
+hdf5_dirname_base = f"/Volumes/Tohoku_15/Fiber-2_HDF5/"
+hdf5_dirname = f"{hdf5_dirname_base}{hdf5_starttime_jst.year}/{hdf5_starttime_jst.month}/{hdf5_starttime_jst.day}/"  #"hdf5/"
+
+used_channel_list = [str(_).zfill(4) for _ in range(700, 705, 5)]   
--- sakuradas_waveletscat/Params.py
+++ sakuradas_waveletscat/Params.py
@@ -14,8 +14,8 @@ fiber = 'nojiri' #'round'
 # hdf5_starttime_jst = datetime.datetime(2023, 12, 1, 0, 0, 0)
 # hdf5_endttime_jst = datetime.datetime(2023, 12, 1, 0, 10, 0)
 
-hdf5_starttime_jst = datetime.datetime(2025, 3, 27, 0, 0, 0)
-hdf5_endttime_jst = datetime.datetime(2025, 3, 28, 0, 0, 0)
+hdf5_starttime_jst = datetime.datetime(2025, 5, 15, 0, 0, 0)
+hdf5_endttime_jst = datetime.datetime(2025, 5, 16, 0, 0, 0)
 
 Nseconds = int( (hdf5_endttime_jst-hdf5_starttime_jst).total_seconds() )
 N_minute = int( (hdf5_endttime_jst - hdf5_starttime_jst).total_seconds() / 60.0 )
@@ -23,6 +23,7 @@ windL = 60.0*N_minute
 low_pass = 0.5
 high_pass = 50
 
-hdf5_dirname = "/Users/hirosetakashi/Volumes/noise_monitoring/noise_monitoring/DAS/Tohoku_15/Fiber-2_HDF5/2025/03/27/"  #"hdf5/"
+hdf5_dirname_base = f"/Volumes/Tohoku_15/Fiber-2_HDF5/"
+hdf5_dirname = f"{hdf5_dirname_base}{hdf5_starttime_jst.year}/{hdf5_starttime_jst.month}/{hdf5_starttime_jst.day}/"  #"hdf5/"
 
 used_channel_list = [str(_).zfill(4) for _ in range(700, 705, 5)]   
